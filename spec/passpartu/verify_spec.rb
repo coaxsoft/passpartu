2b26b857e1c77fb6fc938d8e2fcc1b9d4711d4e6
Orest Falchuk <orest@Orests-iMac.local>
2019-03-15 16:52:56 +0200

    Fix raise error on crud

--- spec/passpartu/verify_spec.rb
+++ spec/passpartu/verify_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe Passpartu::Verify do
   describe '#call' do
     let(:policy_class) { Passpartu.config.policy_class }
 
+    before do
+      Passpartu.config.raise_policy_missed_error = true
+    end
+
     context 'for admin' do
       let(:role) { 'admin' }
 
@@ -73,6 +77,48 @@ RSpec.describe Passpartu::Verify do
 
           it 'overrides crud true and returns false for orders delete' do
             expect(described_class.call(role, %i[items delete])).to eq false
+          end
+
+          context 'misspelled' do
+            context 'raise policy_missed_error true' do
+              it 'raises an error if its not crud action' do
+                expect { described_class.call(role, %i[items crea]) }.to raise_error Passpartu::ValidateResult::PolicyMissedError
+              end
+
+              it 'raises an error if its not crud action' do
+                expect { described_class.call(role, %i[items rea]) }.to raise_error Passpartu::ValidateResult::PolicyMissedError
+              end
+
+              it 'raises an error if its not crud action' do
+                expect { described_class.call(role, %i[items upde]) }.to raise_error Passpartu::ValidateResult::PolicyMissedError
+              end
+
+              it 'raises an error if its not crud action' do
+                expect { described_class.call(role, %i[items dele]) }.to raise_error Passpartu::ValidateResult::PolicyMissedError
+              end
+            end
+
+            context 'raise policy_missed_error false' do
+              before(:each) do
+                Passpartu.config.raise_policy_missed_error = false
+              end
+
+              it 'returns false for orders create' do
+                expect(described_class.call(role, %i[items crea])).to eq false
+              end
+
+              it 'returns false for orders read' do
+                expect(described_class.call(role, %i[items rea])).to eq false
+              end
+
+              it 'returns false for orders update' do
+                expect(described_class.call(role, %i[items upde])).to eq false
+              end
+
+              it 'returns false for orders delete' do
+                expect(described_class.call(role, %i[items dele])).to eq false
+              end
+            end
           end
         end
       end
