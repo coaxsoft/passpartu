3032fc69d807b5b8bbc64d90bcb698a2a3ffc1d2
Orest Falchuk <orest@Orests-iMac.local>
2019-03-20 13:20:15 +0200

    Add per role methods && block execution

--- lib/passpartu/patcher.rb
+++ lib/passpartu/patcher.rb
@@ -11,8 +11,14 @@ module Passpartu
 
     def call
       klass.class_eval do
-        define_method('can?') do |*keys, except: nil|
-          Passpartu::Verify.call(role, keys, except: except)
+        define_method('can?') do |*keys, except: nil, &block|
+          Passpartu::BlockVerify.call(role, keys, except: except, &block)
+        end
+
+        Passpartu.policy.keys.each do |policy_role|
+          define_method("#{policy_role}_can?") do |*keys, except: nil, &block|
+            role.to_s == policy_role && Passpartu::BlockVerify.call(role, keys, except: except, &block)
+          end
         end
       end
     end
