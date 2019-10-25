64b2e8aeafcb084656d4511830ef90a93bd38f0e
Yurii Verbytskyi <yuriy.v@coaxsoft.com>
2019-10-25 13:54:43 +0300

    typo fix

--- lib/passpartu/initializer.rb
+++ lib/passpartu/initializer.rb
@@ -4,13 +4,13 @@ module Passpartu
   module Initializer
     HOME_PATH = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
     CONFIG_FILE_TEMPLATE = File.join(HOME_PATH, 'config', 'passpartu_template.yml')
-    INITITALIZER_FILE_TEMPLATE = File.join(HOME_PATH, 'config', 'initializers', 'passpartu.rb')
+    INITIALIZER_FILE_TEMPLATE = File.join(HOME_PATH, 'config', 'initializers', 'passpartu.rb')
     DEFAULT_CONFIG_FILE_PATH = './config/passpartu.yml'
     DEFAULT_INITIALIZER_PATH = './config/initializers/passpartu.rb'
 
     def check_or_create_defaults(policy_file)
       File.file?(policy_file) || clone_file(DEFAULT_CONFIG_FILE_PATH, CONFIG_FILE_TEMPLATE)
-      File.file?(DEFAULT_INITIALIZER_PATH) || clone_file(DEFAULT_INITIALIZER_PATH, INITITALIZER_FILE_TEMPLATE)
+      File.file?(DEFAULT_INITIALIZER_PATH) || clone_file(DEFAULT_INITIALIZER_PATH, INITIALIZER_FILE_TEMPLATE)
     end
 
     private
