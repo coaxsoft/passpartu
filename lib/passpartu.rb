29cdad7ed6350e67ab50489b09c5bcf097718d61
Yurii Verbytskyi <yuriy.v@coaxsoft.com>
2019-10-25 14:21:11 +0300

    Fix Issue #11: `gem crashes if no default config file present`

--- lib/passpartu.rb
+++ lib/passpartu.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'passpartu/version'
 require 'yaml'
 require_relative 'passpartu/patcher'
@@ -5,6 +7,7 @@ require_relative 'passpartu/verify'
 require_relative 'passpartu/block_verify'
 require_relative 'passpartu/validate_result'
 require_relative 'passpartu/user' # for testing only
+require_relative 'passpartu/initializer'
 
 module Passpartu
   class Error < StandardError; end
@@ -27,23 +30,44 @@ module Passpartu
   end
 
   class Config
+    include Passpartu::Initializer
+
     attr_accessor :policy, :raise_policy_missed_error
-    attr_reader :policy_file
+    attr_reader :policy_file, :use_custom_config_file
 
     def initialize
-      @policy_file = './config/passpartu.yml'
+      @policy_file = DEFAULT_CONFIG_FILE_PATH
+      check_or_create_defaults(@policy_file) unless @use_custom_config_file
       @policy = YAML.load_file(policy_file)
       @raise_policy_missed_error = true
+      @use_custom_config_file = false
     end
 
     def policy_file=(file = nil)
-      @policy_file = file || './config/passpartu.yml'
+      @use_custom_config_file = File.file?(file)
+      @policy_file = file || DEFAULT_CONFIG_FILE_PATH
       @policy = YAML.load_file(policy_file)
+    end
+
+    def validate_policy
+      raise wrong_config_error unless policy.is_a?(Hash)
+    end
+
+    private
+
+    def wrong_config_error
+      <<-ERROR
+
+        ************************************************************************************
+        !!! Passpartu is not configured properly. Check configuration file: #{@policy_file}
+        :: Configuration example: https://github.com/coaxsoft/passpartu#real-life-example ::
+        ************************************************************************************
+      ERROR
     end
   end
 
   configure {}
 end
 
-initializer = './config/initializers/passpartu.rb'
-require initializer if File.exist?(initializer)
+require Passpartu::Initializer::DEFAULT_INITIALIZER_PATH
+Passpartu.config.validate_policy
--- lib/passpartu/user.rb
+++ lib/passpartu/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # for testing only
 
 module Passpartu
