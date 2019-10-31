9659dc8899da7f2e9a6f0d5d04328df85de4f7f6
Yurii Verbytskyi <yuriy.v@coaxsoft.com>
2019-10-31 14:44:48 +0200

    Create config folders unless exist

--- lib/passpartu/initializer.rb
+++ lib/passpartu/initializer.rb
@@ -5,10 +5,13 @@ module Passpartu
     HOME_PATH = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
     CONFIG_FILE_TEMPLATE = File.join(HOME_PATH, 'config', 'passpartu_template.yml')
     INITIALIZER_FILE_TEMPLATE = File.join(HOME_PATH, 'config', 'initializers', 'passpartu.rb')
-    DEFAULT_CONFIG_FILE_PATH = './config/passpartu.yml'
-    DEFAULT_INITIALIZER_PATH = './config/initializers/passpartu.rb'
+    DEFAULT_CONFIG_FOLDER_PATH = './config'
+    DEFAULT_INITIALIZER_FOLDER_PATH = './config/initializers'
+    DEFAULT_CONFIG_FILE_PATH = File.join(DEFAULT_CONFIG_FOLDER_PATH, 'passpartu.yml')
+    DEFAULT_INITIALIZER_PATH = File.join(DEFAULT_INITIALIZER_FOLDER_PATH, 'passpartu.rb')
 
     def check_or_create_defaults(policy_file)
+      mkdirs_if_not_exists DEFAULT_CONFIG_FOLDER_PATH, DEFAULT_INITIALIZER_FOLDER_PATH
       File.file?(policy_file) || clone_file(DEFAULT_CONFIG_FILE_PATH, CONFIG_FILE_TEMPLATE)
       File.file?(DEFAULT_INITIALIZER_PATH) || clone_file(DEFAULT_INITIALIZER_PATH, INITIALIZER_FILE_TEMPLATE)
     end
@@ -28,6 +31,12 @@ module Passpartu
     end
 
     private
+
+    def mkdirs_if_not_exists(*dir_names)
+      dir_names.each do |name|
+        Dir.mkdir(name) unless Dir.exist?(name)
+      end
+    end
 
     def clone_file(destination, source)
       File.write(destination, File.read(source))
